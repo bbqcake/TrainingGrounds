a50edf000d976a4938d345618cb3ff705c9b03a0
bbqcake <tdarkture@gmail.com>
2018-04-24 22:57:35 +0200

    300

--- Source/TrainingGrounds/GrassComponent.cpp
+++ Source/TrainingGrounds/GrassComponent.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "TrainingGrounds.h"
 #include "GrassComponent.h"
+#include "TrainingGrounds.h"
+
 
 
 // Sets default values for this component's properties
